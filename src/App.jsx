4c3110e259ca2b775df6a3da4ce72c55e03382ee
kevinvillajim <kevinvillajim@hotmail.com>
2025-07-01 11:15:53 -0500

    Reupload

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Home from "./views/Home";
 import Nosotros from "./views/Nosotros";
 import Contacto from "./views/Contacto";
@@ -10,52 +10,25 @@ import Tecnologia from "./views/Tecnologia";
 import "./App.css";
 
 function App() {
-  return (
-    <>
-      {/* <ThemeProvider theme={theme}> */}
-      <Router>
-        <Routes>
-          <Route
-            path="/home"
-            element={<Home />}
-          />
-          <Route
-            path="/nosotros"
-            element={<Nosotros />}
-          />
-          <Route
-            path="/contacto"
-            element={<Contacto />}
-          />
-          <Route
-            path="/trabajo"
-            element={<Trabajo />}
-          />
-          <Route
-            path="/pantallas"
-            element={<Pantallas />}
-          />
-          <Route
-            path="/ciberseguridad"
-            element={<Ciberseguridad />}
-          />
-          <Route
-            path="/seguridad-electronica"
-            element={<Seguridad />}
-          />
-          <Route
-            path="/tecnologia"
-            element={<Tecnologia />}
-          />
-          <Route
-            path="/"
-            element={<Home />}
-          />
-        </Routes>
-      </Router>
-      {/* </ThemeProvider> */}
-    </>
-  );
+	return (
+		<>
+			{/* <ThemeProvider theme={theme}> */}
+			<Router basename="/bconnect-ec">
+				<Routes>
+					<Route path="/home" element={<Home />} />
+					<Route path="/nosotros" element={<Nosotros />} />
+					<Route path="/contacto" element={<Contacto />} />
+					<Route path="/trabajo" element={<Trabajo />} />
+					<Route path="/pantallas" element={<Pantallas />} />
+					<Route path="/ciberseguridad" element={<Ciberseguridad />} />
+					<Route path="/seguridad-electronica" element={<Seguridad />} />
+					<Route path="/tecnologia" element={<Tecnologia />} />
+					<Route path="/" element={<Home />} />
+				</Routes>
+			</Router>
+			{/* </ThemeProvider> */}
+		</>
+	);
 }
 
 export default App;
